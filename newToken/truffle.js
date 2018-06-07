acd4ed4c414361bbf727696c17be9de0129c7647
Franco <scucchiero@gmail.com>
2018-06-07 12:06:34 -0300

    Updating..

--- newToken/truffle.js
+++ newToken/truffle.js
@@ -5,17 +5,17 @@ module.exports = {
   networks: {
     development: {
       host: "localhost",
-      port: 7545,
-      gas: 6500000,
-      network_id: "5777"
-    } ,
+      port: 8545,
+      gas:3000000,
+      network_id: "*"
+    } /*,
 
     ropsten: {
         provider: new HDWalletProvider("castle dwarf arrange case guide hello involve mom budget ethics scheme ribbon", "https://ropsten.infura.io/VDPJVXBhW7ruxTPjDagg"),
         network_id: 3,
         gas: 4500000
     },
-    
+    */
   }
   
 };
