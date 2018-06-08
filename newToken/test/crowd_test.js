a4f02584a5bf6ae780cdf12adb2eb2f9231d8276
Franco <scucchiero@gmail.com>
2018-06-08 16:49:29 -0300

    Some problems solved, optimized testing features but some problems found

--- newToken/test/crowd_test.js
+++ newToken/test/crowd_test.js
@@ -27,7 +27,7 @@ contract("Scoin", async (accounts) => {
   it("Allowing crowdsale to buy tokens", async () => {
     var scoin = await Scoin.deployed();
     var crowd = await Crowdsale.deployed();
-    scoin.setAllowed(crowd.address);
+    await scoin.setAllowed(crowd.address);
     assert.equal(await scoin.allowed(),crowd.address);
     });
 
@@ -41,18 +41,21 @@ contract("Scoin", async (accounts) => {
     var buyer = web3.eth.accounts[2];
     
     var amount=100;
-    var confirm = false;
-
-    
-    await scoin.setAllowed(crowd.address);
+    var confirmer = false;   
 
     var initialBalance_buyer= await web3.eth.getBalance(buyer);
     var initialBalance_wallet= await web3.eth.getBalance(wallet);
  
-    await crowd.buyTokens(beneficiary,{from:buyer,value:amount});
+    var receipt = await crowd.buyTokens(beneficiary,{from:buyer,value:amount});
+    var tx = web3.eth.getTransaction(receipt.tx);
+    var estimatedGas = new web3.BigNumber(receipt.receipt.gasUsed);
+    estimatedGas = estimatedGas.mul(new web3.BigNumber(tx.gasPrice));
+
     var finalBalance_wallet= await web3.eth.getBalance(wallet);
-    
-    if ( Number(await web3.eth.getBalance(buyer)) < Number(initialBalance_buyer)-Number(amount)) confirmer = true;
+    var currentBalance=Number(await web3.eth.getBalance(buyer));
+    var expectedBalance = Number(initialBalance_buyer)-Number(amount)-Number(estimatedGas);
+ 
+    if ( currentBalance == expectedBalance )  confirmer = true; 
     assert.equal( Number(finalBalance_wallet),Number(initialBalance_wallet)+Number(amount));
     assert.equal(confirmer,true);
 
@@ -62,15 +65,13 @@ contract("Scoin", async (accounts) => {
     var crowd = await Crowdsale.deployed();   
     
     var token = scoin.address;
-    var wallet = web3.eth.accounts[0];
-    var beneficiary = web3.eth.accounts[1];
+    var wallet = web3.eth.accounts[6];
+    var beneficiary = web3.eth.accounts[5];
     var buyer = web3.eth.accounts[2];
     
     var amount=10000;
-    var confirm = false;
-
-    
-    await scoin.setAllowed(crowd.address);
+    var confirmer = false;
+   
     await crowd.buyTokens(beneficiary,{from:buyer,value:amount}); // tokens has arrived to beneficiary
 
     var initialBalance_beneficiary= await scoin.balanceOf(beneficiary);
@@ -80,7 +81,7 @@ contract("Scoin", async (accounts) => {
 
     var finalBalance_wallet= await scoin.balanceOf(wallet); 
     var finalBalance_beneficiary= await scoin.balanceOf(beneficiary);
- 
+
     if ( Number(finalBalance_beneficiary) <= Number(finalBalance_beneficiary)-Number(99*initialBalance_beneficiary/100)) confirmer = true;
     assert.equal( Number(finalBalance_wallet),Number(initialBalance_wallet)+Number(99*initialBalance_beneficiary/100));
     assert.equal(confirmer,true);
@@ -98,10 +99,8 @@ contract("Scoin", async (accounts) => {
     var buyer = web3.eth.accounts[2];
     
     var amount=10000;
-    var confirm = false;
-    
-    
-    await scoin.setAllowed(crowd.address);
+    var confirmer = false;
+
     await scoin.finishMinting();
     
     var initialBalance_wallet = await scoin.balanceOf(web3.eth.accounts[5]);
